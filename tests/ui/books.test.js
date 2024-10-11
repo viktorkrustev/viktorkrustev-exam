7314e7355269038a962521d16ab72053549ce821
viktorkrustev <viktorkrustev03@abv.bg>
2024-10-12 01:55:50 +0300

    url fix

--- tests/ui/books.test.js
+++ tests/ui/books.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check books page', async ({ page }) => {
-    await page.goto('https://viktorkrustev-exam.onrender.com/bookd');
+    await page.goto('https://viktorkrustev-exam.onrender.com/books');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
--- tests/ui/home.test.js
+++ tests/ui/home.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check home page', async ({ page }) => {
-    await page.goto('https://viktorkrustev-exam.onrender.com/home');
+    await page.goto('https://viktorkrustev-exam.onrender.com/');
     const heading = await page.$('h1');
     const text = await heading.textContent();
     expect(text).toContain('Books Collection');  
